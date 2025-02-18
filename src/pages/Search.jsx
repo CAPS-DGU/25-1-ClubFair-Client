ff5e66921d93f65258b32e0d964624370b904a58
wonjongin <wji0227@naver.com>
2025-02-19 00:36:50 +0900

    style: Add search page

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,16 +1,34 @@
+import { useState } from "react";
+import SearchBar from "../components/SearchBar";
 import WikiCard from "../components/WikiCard";
+import "./Search.css";
 
 export default function Search() {
+  const [search, setSearch] = useState("");
+
   return (
     <div>
-      <h1>Search</h1>
-      <p>Welcome to the search page.</p>
-      <WikiCard
-        title="누군가"
-        enteredIn={23}
-        college="첨단융합대학"
-        department="컴퓨터공학전공"
+      <SearchBar
+        value={search}
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
       />
+      <div className="search-result-info">&quot;{search}&quot; 검색 결과</div>
+      <div className="search-card-container">
+        <WikiCard
+          title="누군가"
+          enteredIn={23}
+          college="첨단융합대학"
+          department="컴퓨터공학전공"
+        />
+        <WikiCard
+          title="누군가"
+          enteredIn={23}
+          college="첨단융합대학"
+          department="컴퓨터공학전공"
+        />
+      </div>
     </div>
   );
 }
