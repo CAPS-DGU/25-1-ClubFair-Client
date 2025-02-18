760673ce4e6b886a32f810f08377fbe670b99d7c
wonjongin <wji0227@naver.com>
2025-02-18 23:45:59 +0900

    style: Add WikiCard

---
+++ src/components/WikiCard.jsx
@@ -0,0 +1,14 @@
+import "./WikiCard.css";
+
+export default function WikiCard({ title, enteredIn, college, department }) {
+  return (
+    <div className="wiki-card">
+      <div className="wiki-card-header">
+        <div className="wiki-card-title">{title}</div>
+        <div className="wiki-card-desc">{enteredIn}학번</div>
+      </div>
+      <div className="wiki-card-desc">{college}</div>
+      <div className="wiki-card-desc">{department}</div>
+    </div>
+  );
+}
--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -1,9 +1,16 @@
+import WikiCard from "../components/WikiCard";
 
 export default function Search() {
   return (
     <div>
       <h1>Search</h1>
       <p>Welcome to the search page.</p>
+      <WikiCard
+        title="누군가"
+        enteredIn={23}
+        college="첨단융합대학"
+        department="컴퓨터공학전공"
+      />
     </div>
-  )
+  );
 }
